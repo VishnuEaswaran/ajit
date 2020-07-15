d13128f6b1d173a42303623e45e6559af6adbcc8
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2020-07-16 00:13:27 +0530

    [vishnu] updated for calling from fft; passing i/p & o/p fixed.

--- divide.s
+++ divide.s
@@ -1,52 +1,56 @@
-! divide and store in heap recursively
+! divide and store in heap 
 
     .global divide_store
     .align 4
 divide_store:
-    clr %g5                 ! count for recursion
-    mv %g7, %l7             ! make a copy to l7
-    ld [%l7], %i5           ! n: size of array
-    add %g5, %l7             ! go to first array element
+    ! incoming
+    ! i5 size of the array
+    ! i4 base address of first element
+    ! i3 base address of current empty point of heap
+    ! i2 count of recursion
     
-recurse:
     save %sp, -96-((8+2)*4) & -8, %sp   ! ((array_size+2empty_elem)*4)bytes 
-    cmp 1, %i5              ! check if n==2
+    cmp %i5, 1              ! check if n==2
     be return               ! return if n==2
         nop                 ! DELAY SLOT
 
-    udiv %i5, 2, %l6        ! n/2 in l6
+    udiv %i5, 2, %l7        ! n/2 in l7
     
     ! for loop
     ba test
-        clr %l5             ! loop counter initialised
+        clr %l6             ! loop counter initialised
 loop:
-    ! get elements from array_input
-    ! copy them to sorresponding arrays
+    ! get elements from passed on array
+    ! copy them to corresponding even & odd arrays
     
-    smul %g5, %l5, %l4        ! (4 * i) stored to l4
+    smul 4, %l6, %l5        ! (4 * i) stored to l5
 
     !even
-    smul 2, %l4, %l3        ! (2*i)*4 stored to l4
-    ld [%g7+%l3], %l2       ! loaded array_input(i*2) to l4
-    st %l2, [%g6+%l4]       ! array_input(i*4) stored to heap(i)
+    smul 2, %l5, %l4        ! (2*i)*4 stored to l4
+    ld [%i4+%l4], %l3       ! loaded array_input(i*2) to l3
+    st %l3, [%i3+%l4]       ! array_input(i*4) stored to heap(i)
 
     !odd
-    add %g5, %l3, %l2       ! ((2*i) + 1)*4 stored in l2
-    ld [%g7+%l2], %l2       ! loaded array_input((2*i)+1) to l2
+    add 4, %l4, %l2         ! ((2*i) + 1)*4 stored in l2
+    ld [%i4+%l2], %l2       ! loaded array_input((2*i)+1) to l2
     smul 4, %g5, %l3        ! since odd starts 4 elements from heap, 4*4 stored in l3
-    add %l4, %l3            ! store ((heap+4)+i)4
-    st %l2, [%g6+%l3]
+    add %i3, %l3, %l3       ! store heap+4 in l3
+    st %l2, [%l3+%l5]       ! store odd element from l2 to (heap+4+i)th memory
 
-    inc %l5                 ! l5++
+    inc %l6                 ! l6++
 
 test:
-    cmp %l5, %l6            ! check i<(n/2)
+    cmp %l6, %l7            ! check i<(n/2)
     bl loop
-        nop    
-
-    ba recurse
-        
+        nop   
 
 return:     
+    ! return values: size, current heap base address, next empty heap address
+    ! return values when cwp+1; in --> out
+    mov %l7, %i5            ! size of the array
+    mov %i3, %i4            ! current base address of heap. even and odd can be
+                            ! accessed by knowing this
+    add 32, %i3, %i3        ! added 8*4 bytes to current heap address for next
+                            ! empty heap
     ret
     restore
